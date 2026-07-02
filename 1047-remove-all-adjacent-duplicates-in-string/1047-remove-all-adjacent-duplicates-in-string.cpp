c8935c5d05f473734a9ec472c785ffbdc5753e31
Ayush <168622573+ayush-uttam@users.noreply.github.com>
2026-07-02 23:22:28 +0530

    Time: 0 ms (100%), Space: 13.7 MB (59.42%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        if(s.size()==0) return s;
         string res="";
-        res+=s[0];
-        for(int i=1;i<s.size();i++){
+        for(int i=0;i<s.size();i++){
             if(!res.empty()&&res.back()==s[i])    res.pop_back();
             else    res+=s[i];
         }
