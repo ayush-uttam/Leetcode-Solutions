d8f8008a3f26107bab564c485a61d3e709ff83b5
Ayush <168622573+ayush-uttam@users.noreply.github.com>
2026-07-04 20:53:22 +0530

    Time: 3 ms (11.89%), Space: 9.4 MB (25.52%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        vector<int>f1(26,0);
-        vector<int>f2(26,0);
+        vector<int>f(26,0);
         for(char x:s){
-            f1[x-'a']++;
+            f[x-'a']++;
         }
         for(char x:t){
-            f2[x-'a']++;
+            f[x-'a']++;
         }
         int i;
-        for(i=0;i<f1.size();i++){
-            if(f1[i]!=f2[i])    break;
+        for(i=0;i<f.size();i++){
+            if(f[i]%2!=0)    break;
         }
         return (char)(i+'a');
     }
