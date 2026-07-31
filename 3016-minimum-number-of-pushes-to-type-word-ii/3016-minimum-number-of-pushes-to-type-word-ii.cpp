8dd9b62d951b423cafda7b088cae4809368be478
Ayush <168622573+ayush-uttam@users.noreply.github.com>
2026-07-31 11:34:54 +0530

    Time: 7 ms (69.76%), Space: 25.4 MB (84.45%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -2,20 +2,16 @@ class Solution{
 public:
     int minimumPushes(string word){
         vector<pair<char,int>>mpp(26);
-        int c=0;
         for(int i=0;i<26;i++){
             mpp[i]={'a'+i,0};
         }
         for(char x:word){
-            if(mpp[x-'a'].second==0)    c++;
             mpp[x-'a'].second++;
         }
         sort(mpp.begin(), mpp.end(), [](auto &a, auto &b){
             return a.second > b.second;
         });
-        int cost=0;
-        int idx=0;
-        int mul=1;
+        int cost=0,idx=0,mul=1;
         for(auto x:mpp){
             if(x.second==0) continue;
             idx++;
